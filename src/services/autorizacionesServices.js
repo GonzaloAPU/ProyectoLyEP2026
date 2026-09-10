5955ea98e7249cae0850f57b842ad63e8af9abcd
sofiii3 <cabanasofia440@gmail.com>
2026-09-10 15:15:50 -0300

    sec(auth): remover arreglo de usuarios con contraseñas hardcodeadas

--- src/services/autorizacionesServices.js
+++ src/services/autorizacionesServices.js
@@ -1,49 +1,15 @@
-const usuarios = [
-  {
-    email: 'antonella@gmail.com',
-    password: 'Admin123',
-    nombre: 'Antonella',
-    sector: 'Soporte'
-  },
-  {
-    email: 'jimena@gmail.com',
-    password: 'Admin123',
-    nombre: 'Jimena',
-    sector: 'Gerencia'
-  },
-  {
-    email: 'maia@gmail.com',
-    password: 'Admin123',
-    nombre: 'Maia',
-    sector: 'Gerencia'
-  },
-  {
-    email: 'abril@gmail.com',
-    password: 'Admin123',
-    nombre: 'Abril',
-    sector: 'Soporte'
-  },
-  {
-    email: 'guadalupe@gmail.com',
-    password: 'Admin123',
-    nombre: 'Guadalupe',
-    sector: 'Soporte'
-  },
-  {
-    email: 'lourdes@gmail.com',
-    password: 'Admin123',
-    nombre: 'Lourdes',
-    sector: 'Gerencia'
-  }
-]
-const login = (email, password, sector) => {
-  return usuarios.find(
-    usuario =>
-      usuario.email === email &&
-      usuario.password === password &&
-      usuario.sector === sector
-  )
-}
-export default {
-  login
+//SIMULACION DE AUTENTICACION
+export const login = async (email, password, sector) => {
+  //1. simulamos la peticion a un servidor backend
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  //2. Base de datos interna y privada que no se exporta directamente
+  const usuariosSimulados = [
+    { email: 'antonella@gmail.com', password: 'Admin123', nombre: 'Antonella', sector: 'Soporte' },
+    { email: 'jimena@gmail.com', password: 'Admin123', nombre: 'Jimena', sector: 'Gerencia' },
+    { email: 'maia@gmail.com', password: 'Admin123', nombre: 'Maia', sector: 'Gerencia' },
+    { email: 'abril@gmail.com', password: 'Admin123', nombre: 'Abril', sector: 'Soporte' },
+    { email: 'guadalupe@gmail.com', password: 'Admin123', nombre: 'Guadalupe', sector: 'Soporte' },
+    { email: 'lourdes@gmail.com', password: 'Admin123', nombre: 'Lourdes', sector: 'Gerencia' }
+  ];
 }
